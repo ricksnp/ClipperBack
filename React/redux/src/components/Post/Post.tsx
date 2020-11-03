5dfb9c9d0cfffd182bfcfe68850f51c9b41c5a8b
Matthew Thomas <matthew.thomas@revature.net>
2020-11-02 21:04:42 -0600

    neyah

---
+++ React/redux/src/components/Post/Post.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Carousel} from './Carousel'
+
+
+export function landingPane(prop:any){
+    return(
+        <div className = "Post" >
+            <a href={prop.user.page}><img src={prop.profPicLink} className="Post-Profile-Pic"/></a>
+            <div> {/* Needs contitional statement for Carousel */}
+                <Carousel prop="prom.images"/>  
+                <p>{prop.post.postText}</p>
+            </div>
+            <button className="like"></button>          
+        </div>
+    )
+}
