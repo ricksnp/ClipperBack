995865ba249af7b88397b7a1b3ebb0fbed6ff814
Matthew Thomas <matthew.thomas@revature.net>
2020-11-05 21:14:59 -0600

    Home page added

--- React/redux/src/App.tsx
+++ React/redux/src/App.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import { Provider } from "react-redux";
 //import { store } from "./Store";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LandingPage from "./views/LandingPage"
+import LandingPage from "./views/LandingPage/LandingPage"
 import LoginPage from "./views/LoginPage";
 import SignupPage from './views/SignupPage'
 import ResetPassword from "./views/ResetPassword"
+import HomePage from "./views/HomePage/HomePage";
 
 function App() {
   return (
-    <div className=" GreyBackground">
-      <div className="col-8 mx-auto ">
+    <div>
+      <div >
         <BrowserRouter>
           <Switch>
             <Route path="/landing-page" component={LandingPage} />
             <Route path="/login" component={LoginPage} />
             <Route path="/signup" component={SignupPage} />
             <Route path="/reset-password" component={ResetPassword} />
+            <Route path="/Home" component={HomePage}/>
           </Switch>
         </BrowserRouter>
       </div>
--- React/redux/src/views/LandingPage/LandingPage.tsx
+++ React/redux/src/views/LandingPage/LandingPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../index.css';
-import { LandingPane } from '../components/LandingPane/LandingPane'
-import { Feed } from '../components/Feed/Feed'
+import '../../index.css';
+import './LandingPage.css';
+import { LandingPane } from '../../components/LandingPane/LandingPane'
+import { Feed } from '../../components/Feed/Feed'
 
 export default function LandingPage() {
     return (
-        <div className="row">
+        <div className="row wrapper GreyBackground noScroll">
             <LandingPane />
             <Feed />
         </div>
