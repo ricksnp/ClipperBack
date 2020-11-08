f26fd8c9912f9d0df5b40e4ae4422537126d239e
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2020-11-07 19:37:33 -0500

    Service/Controller

--- Java/Clipper/src/main/java/com/clipper/Driver.java
+++ Java/Clipper/src/main/java/com/clipper/Driver.java
@@ -1,9 +1,16 @@
 package com.clipper;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.clipper.dao.PostDao;
 import com.clipper.dao.UserDao;
+import com.clipper.model.Like;
+import com.clipper.model.Post;
+import com.clipper.model.PostImage;
 import com.clipper.model.User;
 
 public class Driver {
@@ -13,11 +20,16 @@ public class Driver {
 		
 		
 		UserDao dao = ac.getBean(UserDao.class);
+		PostDao pdao = ac.getBean(PostDao.class);
+		
+		
 		
 		dao.save(new User(0, "nathan1", "AAA", "Nathan", "Ricks", "ricks@gmail.com", "lol bio", "pfp link", null, null));
-		dao.save(new User(0, "user2", "password", "User", "Name", "user@nothing.net", "biography", "profile pic", null, null));
+		//dao.save(new User(0, "user2", "password", "User", "Name", "user@nothing.net", "biography", "profile pic", null, null));
 		
-		dao.delete(1);
+		pdao.save(new Post(0, "Hello World", dao.findById(2), null, null));
+		
+		//dao.delete(1);
 		
 		System.out.println(dao.findAll());
 		
