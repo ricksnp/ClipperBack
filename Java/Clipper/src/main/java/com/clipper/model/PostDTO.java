7d20c8c52e26b22d2c3f2ab7cc40e0b7a2170e81
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2020-11-11 23:34:51 -0500

    S3

--- Java/Clipper/src/main/java/com/clipper/model/PostDTO.java
+++ Java/Clipper/src/main/java/com/clipper/model/PostDTO.java
@@ -1,29 +1,43 @@
 package com.clipper.model;
 
+
 public class PostDTO {
 	private int user_id;
 	private String content;
+	private byte[] arr;
 	
-	public PostDTO(int user_id, String content) {
+	public PostDTO(int user_id, String content, byte[] arr) {
 		super();
 		this.user_id = user_id;
 		this.content = content;
+		this.arr = arr;
 	}
-	public PostDTO() {}
-	
+
 	public int getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
 
+	public byte[] getArr() {
+		return arr;
+	}
+
+	public void setFile(byte[] arr) {
+		this.arr = arr;
+	}
+	
+	
 	
 	
 }
---
+++ Java/Clipper/src/main/java/com/clipper/util/S3Uploader.java
@@ -0,0 +1,43 @@
+package com.clipper.util;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import javax.imageio.ImageIO;
+
+public class S3Uploader {
+	
+	private static final String bucketName = "clipperrev";
+	private static final BasicAWSCredentials creds = new BasicAWSCredentials("AKIA5GWYDREO5PTJHRDF", "zN9sC8pESpmc9MrzJXdFq0KVxs6AjooQd0eEfFla");
+	private static final AmazonS3 s3 = AmazonS3Client
+					.builder()
+					.withRegion("us-east-1")
+					.withCredentials(new AWSStaticCredentialsProvider(creds))
+					.build();
+	
+	public S3Uploader() {}
+	
+	public static String upload(byte[] bytes) {
+		String subDirectory = "Images/";
+		String random = Utilities.getSaltString();
+		String fileNameWithPath = subDirectory + random;
+		
+		 try {
+		  File f = File.createTempFile("i", ".png"); 
+	      BufferedImage img = ImageIO.read(new ByteArrayInputStream(bytes));
+	      ImageIO.write(img, "png", f);
+	
+	      s3.putObject(new PutObjectRequest(bucketName, fileNameWithPath, f));	
+	        
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
